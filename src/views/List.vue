8cf6d4eeeeb6716121cb8ae9c5c0f30dd03aace1
kristina <christychri0@gmail.com>
2021-03-29 17:27:17 +0700

    changed About.vue to List.vue

--- src/views/List.vue
+++ src/views/List.vue
@@ -1,5 +1,29 @@
 <template>
-  <div class="list">
-    <h1>This is an about page</h1>
+  <div>
+    <input type="text" v-model="newContact" placeholder="Name" />
+    <button @click="addContact">Add Contact</button>
+
+    <ul>
+      <li v-for="contact in contacts" :key="contact">
+        {{ contact }}
+      </li>
+    </ul>
   </div>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      newContact: '',
+      contacts: ['Beau Thabeast', 'Cindy Rella', 'Alice Vunderlind']
+    }
+  },
+  methods: {
+    addContact() {
+      this.contacts.push(this.newContact)
+      this.newContact = ''
+    }
+  }
+}
+</script>
