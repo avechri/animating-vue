4129148fa9c6df5ecb60fb5d58959423ee86ca7b
kristina <christychri0@gmail.com>
2021-03-29 16:28:28 +0700

    changed "Home page" to "Modal page"

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,6 @@
 <template>
-  <div class="home">
+  <div>
+    <h1>This is the modal page</h1>
     <button @click="toggleModal">Open</button>
     <transition name="fade">
       <div v-if="isOpen" class="modal">
